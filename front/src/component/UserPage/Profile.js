af85eea0fe2bb3775ab5a3c89db0f06bd2badb21
chirab <olivier.chane@epitech.eu>
2019-12-15 09:34:39 +0100

    [ADD] - background recruteur side and input were changed cause it was the same as the user page

---
+++ front/src/component/UserPage/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react'
+
+
+export default class Profile extends Component {
+    render() {
+        return (
+            <div style={{backgroundColor : "red"}}>
+                <p>
+                    hello
+                </p>
+            </div>
+        )
+    }
+}
